540360a3cd0de3707da77c5336d29e57c0f422f1
Khakim Bakhramov <khakimbakhramov@gmail.com>
2025-04-27 13:08:16 +0500

    edit

--- src/component/question/settings/QuestionSettings.tsx
+++ src/component/question/settings/QuestionSettings.tsx
@@ -1,6 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useDeleteQuestionByIdMutation } from "../../../app/api/questionApi.ts";
 import { toast } from "react-toastify";
+import { useTranslation } from "react-i18next";
 
 interface QuestionSettingsProps {
     id: string;
@@ -10,12 +11,13 @@ export const QuestionSettings = ({ id }: QuestionSettingsProps) => {
     const navigate = useNavigate();
     const { templateId } = useParams();
     const [deleteQuestion, { isLoading: isQuestionDeleting }] = useDeleteQuestionByIdMutation();
+    const { t } = useTranslation();
 
     const handleDelete = () => {
         deleteQuestion(id)
             .unwrap()
-            .then(() => toast("Question deleted"))
-            .catch(() => toast("Something went wrong"));
+            .then(() => toast(t("success.deleted")))
+            .catch(() => toast(t("error.common")));
     }
 
     return (
@@ -30,7 +32,8 @@ export const QuestionSettings = ({ id }: QuestionSettingsProps) => {
             <button
                 type="button"
                 className="button-theme"
-                onClick={handleDelete} disabled={isQuestionDeleting}
+                onClick={handleDelete}
+                disabled={isQuestionDeleting}
             >
                 <i className="bi bi-trash3"></i>
             </button>
